3cc0720f93f7d209a79acf7100977b06973dd411
Mike Hodgson <mikehhodgson@users.noreply.github.com>
2018-07-22 19:25:52 +0800

    init commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav({edge: 'right'});
+    $('.parallax-container-no-js').removeClass('parallax-container-no-js');
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
